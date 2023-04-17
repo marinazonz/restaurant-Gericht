0bacd5a0227a68aca15b29693aa2e738e047a60e
Marina Zonova <114925418+marinazonz@users.noreply.github.com>
2023-04-17 17:58:23 +0200

    Finished with css

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import { SubHeading } from "../../components/index";
 
-const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+const Newsletter = () => {
+    return (
+        <div className='app__newsletter'>
+            <div className='app__newsletter-heading'>
+                <SubHeading title='Newsletter' />
+                <h1 className='headtext__cormorant'>
+                    Subscribe to Our Newsletter
+                </h1>
+                <p className='p__opensans'>And never miss latest Updates!</p>
+            </div>
+
+            <div className='app__newsletter-input flex__center'>
+                <input type='email' placeholder='Enter your email address' />
+                <button type='button' className='custom__button'>
+                    Subscribe
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default Newsletter;
--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const FindUs = () => (
-  <div>
-    FindUs
-  </div>
-);
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./FindUs.css";
+
+const FindUs = () => {
+    return (
+        <div className='app__bg section__padding app__wrapper' id='contact'>
+            <div className='app__wrapper_info app__findus-info'>
+                <SubHeading title='Contact' />
+                <h1 className='headtext__cormorant'>Find Us</h1>
+
+                <div className='app__wrapper-content'>
+                    <p className='p__opensans'>
+                        Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9
+                        7SG
+                    </p>
+                    <h2 className='p__cormorant'>Opening hours</h2>
+                    <p className='p__opensans'>
+                        Mon - Fri: 10:00 am - 02:00 am
+                    </p>
+                    <p className='p__opensans'>
+                        Sat - Sun: 10:00 am - 03:00 am
+                    </p>
+                </div>
+                <button type='button' className='custom__button'>
+                    Visit Us
+                </button>
+            </div>
+
+            <div className='app__wrapper_img'>
+                <img src={images.findus} alt='cocktail' />
+            </div>
+        </div>
+    );
+};
 
 export default FindUs;
